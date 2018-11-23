7afec8f773bff4de14938d057fd0fccab7e24624
Robert Kaye <rob@musicbrainz.org>
2018-11-24 00:01:55 +0100

    Adding more and improving tests

---
+++ immutableweb/tests/io_tests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import key
+import stream
+from nose.tools import assert_equals
+import unittest
+
+class TestStreamFileIO(unittest.TestCase):
+
+    def test_file_existence(cls):
+        private_key, public_key = key.make_key_pair()
+        with open("__test-public.pem", "wb") as f:
+            f.write(key.get_public_key_pem(public_key))
+        with open("__test-private.pem", "wb") as f:
+            f.write(key.get_private_key_pem(private_key))
+
+        s = stream.Stream()
+        s.set_stream_signature_keys("__test-public.pem", "__test-private.pem")
+        cls.assertRaises(IOError, s.create, "__test.im", { 'foo' : "bar" })
+
+
+    def test_file_existence_override(cls):
+        private_key, public_key = key.make_key_pair()
+        with open("__test-public.pem", "wb") as f:
+            f.write(key.get_public_key_pem(public_key))
+        with open("__test-private.pem", "wb") as f:
+            f.write(key.get_private_key_pem(private_key))
+
+        s = stream.Stream()
+        s.set_stream_signature_keys("__test-public.pem", "__test-private.pem")
+        try:
+            s.create("__test.im", { 'foo' : "bar" }, force=True)
+        except IOError as err:
+            cls.fail("Force overwrite file dowes not throw the expected exception.")
+            return
+
+
+    def test_missing_file(cls):
+        s = stream.Stream()
+        cls.assertRaises(FileNotFoundError, s.open, "__doesnotexist.im")
--- immutableweb/tests/key_tests.py
+++ immutableweb/tests/key_tests.py
@@ -10,6 +10,7 @@ class TestKeys(unittest.TestCase):
         s = stream.Stream()
         cls.assertRaises(stream.MissingKey, s.create, "__test.im", { 'foo' : 'bar' }, force=True)
 
+
     def test_mismatched_keys(cls):
         private_key, public_key = key.make_key_pair()
         with open("__test-public.pem", "wb") as f:
@@ -25,3 +26,22 @@ class TestKeys(unittest.TestCase):
 
         s = stream.Stream()
         cls.assertRaises(stream.InvalidKeyPair, s.set_stream_signature_keys, "__test-public.pem", "__test2-private.pem")
+
+
+    def test_missing_key_files(cls):
+        private_key, public_key = key.make_key_pair()
+        with open("__test-public.pem", "wb") as f:
+            f.write(key.get_public_key_pem(public_key))
+        with open("__test-private.pem", "wb") as f:
+            f.write(key.get_private_key_pem(private_key))
+
+        private_key2, public_key2 = key.make_key_pair()
+        with open("__test2-public.pem", "wb") as f:
+            f.write(key.get_public_key_pem(public_key2))
+        with open("__test2-private.pem", "wb") as f:
+            f.write(key.get_private_key_pem(private_key2))
+
+        s = stream.Stream()
+        cls.assertRaises(FileNotFoundError, s.set_stream_signature_keys, "<doesnotexist>", "<neither does this>")
+
+#        s.set_stream_signature_keys("__test-public.pem", "__test-private.pem")
