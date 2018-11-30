900110f4aa59ae26747042dfb480e12229f8dfd5
Robert Kaye <rob@musicbrainz.org>
2018-11-30 19:26:38 +0100

    Install the iw binary.

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from distutils.core import setup
 setup(name='Immutable Web',
       version='0.1',
       description='Python modules for creating immutable web streams',
+      long_description=open('README.md').read(),
       author='Robert Kaye',
       author_email='mayhem@gmail.com',
-      url='',
+      url='https://github.com/immutableweb/immutableweb-core',
       packages=['immutableweb'],
+      scripts=['bin/iw']
      )
